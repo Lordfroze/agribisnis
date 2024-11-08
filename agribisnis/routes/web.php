b88f5f89db37045b3e40bf7dab87cb38abc780e2
Lordfroze <yogaadiprtama22@gmail.com>
2024-11-08 20:24:41 +0700

    add:bootstrap

--- agribisnis/routes/web.php
+++ agribisnis/routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
-    // return view('welcome');
     return view('home');
+});
+
+Route::get('/blog',[BlogController::class, 'index']);
+
+Route::get('/laravel', function () {
+    return view('welcome');
+});
+
+Route::get('/yoga', function () {
+    return view('landingpage.index');
 });
